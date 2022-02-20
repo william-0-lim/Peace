c0e1e3feccacaf1dfd1566daf497d1e921f6c243
William Lim <42948215+william-0-lim@users.noreply.github.com>
2022-02-19 21:03:27 -0800

    Added Main Menu
    Added Main Menu as well as adding some images found on internet

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-import pygame
+import pygame as pg
 from pygame.locals import *
 from world import *
 from player import *
+from button import *
 
 # Global Variables
 screen_width = 1000
 screen_height = 1000
 clock = pygame.time.Clock()
 fps = 60
+main_menu = True 
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Peace')
+pygame.display.set_caption('Get Bob Home')
 
 #load images
 sun_img = pygame.image.load('img/background/sun.png')
 bg_img = pygame.image.load('img/background/sky.png')
+# start_img = pg.transform.scale(pygame.image.load('img/buttons/start.png'), (200,100))
+# exit_img = pg.transform.scale(pygame.image.load('img/buttons/quit.png'), (200,100))
+start_img = pygame.image.load('img/buttons/start_btn.png')
+exit_img = pygame.image.load('img/buttons/exit_btn.png')
+space_img = pygame.image.load('img/background/space.png')
+menu_img = pygame.image.load('img/background/m.png')
+
 
 #Level1
 world_data = [
@@ -40,22 +49,42 @@ world_data = [
 [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-# world = World(world_data)
+
 player = Player(100, screen_height - 130)
 world = World(world_data)
 pygame.init()
+
+# Create Buttons
+start_button = Button(screen_width // 2 - 350, screen_height // 2, start_img)
+exit_button = Button(screen_width // 2 + 150, screen_height // 2, exit_img)
+
+# Texts
+font = pygame.font.Font('Fontrust.otf', 125)
+font1 = pygame.font.Font('Fontrust.otf', 30)
+title = font.render('Get Bob Home', True, (0,0,0))
+credit = font1.render('made by TaeGyun Lim', True, (0,0,0))
 
 run = True
 while run:
     # sets the frame rate
     clock.tick(fps)
 
+    
     # create image on the screen
     screen.blit(bg_img, (0, 0))
     screen.blit(sun_img, (100, 100))
 
-    world.draw(screen)
-    player.update(screen_height, screen, world)
+    if main_menu == True:
+        screen.blit(title, (250,125))
+        screen.blit(credit, (400,900))
+        if exit_button.draw(screen):
+            run = False
+        if start_button.draw(screen):
+            main_menu = False
+    else:
+
+        world.draw(screen)
+        player.update(screen_height, screen, world)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
